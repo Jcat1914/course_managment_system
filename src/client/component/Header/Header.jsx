707d68619f3b4844220f26b4279d98dcea798b27
Juan Acuna <juanacuna@Juans-Mac.local>
2024-05-11 22:38:55 -0600

    Add Navigation Button

--- src/client/component/Header/Header.jsx
+++ src/client/component/Header/Header.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import KeiserLogo from "../Logos/KeiserLogo";
 import { LogoutButton } from "../Logout/LogoutButton";
+import { PrivateRoutes } from "../../models/routes";
+import { Link } from "react-router-dom";
 
-const Header = () => {
+function Header({ onClick, dashboardText, role }) {
   return (
     <header className="bg-blue-200 px-8">
       <nav className="flex justify-between items-center p-2">
@@ -10,7 +12,10 @@ const Header = () => {
           <KeiserLogo />
           <h1 className="text-lg font-mono">Course Scheduling System</h1>
         </div>
-        <div>
+        <div className="flex gap-8 items-center">
+          {role === 'admin' ? (
+            <button onClick={onClick}><Link to={PrivateRoutes.ADMIN}>{dashboardText}</Link></button>
+          ) : null}
           <LogoutButton />
         </div>
       </nav>
